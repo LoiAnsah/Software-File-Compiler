929165cfab96731d30c492fb51338350f64eaecb
NikaKolin <138161406+NikaKolin@users.noreply.github.com>
2024-03-12 13:13:31 -0400

    Update UploadFrame.java
    GUI frame

--- CP317/Cp317/UploadFrame.java
+++ CP317/Cp317/UploadFrame.java
@@ -1,20 +1,43 @@
 package CP317.src;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 
-public class FilesUtilites {
-    public static void UploadFile(final File file) {
-        try {
-            Scanner SupplyFileIn = new Scanner(file);
-            String line;
-            while (SupplyFileIn.hasNext()) {
-                line = SupplyFileIn.nextLine();
-                System.out.println(line);
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class UploadFrame extends JFrame implements ActionListener {
+    private final JButton button=new JButton("Upload File");
+    UploadFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+        this.setTitle("Upload File");
+        this.setLocationRelativeTo(null);
+        this.setLayout(new FlowLayout());
+        
+        button.setFocusable(false);
+        button.addActionListener(this);
+
+        this.add(button);
+
+        this.pack();
+        this.setVisible(true);
+    }
+  
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if (e.getSource()==button) {
+            JFileChooser fileupload = new JFileChooser();
+            int response = fileupload.showOpenDialog(null);
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileupload.getSelectedFile().getAbsolutePath());
+                FilesUtilites.UploadFile(file);
             }
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
         }
     }
 }
