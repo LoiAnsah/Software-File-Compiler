a6eb1530ff3f70e76ef0494e9305225dec91a05f
NikaKolin <138161406+NikaKolin@users.noreply.github.com>
2024-03-13 10:32:27 -0400

    Update UploadFrame.java
    Updated UploadFrame (where the button events are and where it's going to access the functions)

--- CP317/Cp317/UploadFrame.java
+++ CP317/Cp317/UploadFrame.java
@@ -1,43 +1,59 @@
 package CP317.src;
 
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class UploadFrame extends JFrame implements ActionListener {
-    private final JButton button=new JButton("Upload File");
-    UploadFrame() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
-        this.setTitle("Upload File");
-        this.setLocationRelativeTo(null);
-        this.setLayout(new FlowLayout());
-        
-        button.setFocusable(false);
-        button.addActionListener(this);
-
-        this.add(button);
-
-        this.pack();
-        this.setVisible(true);
-    }
-  
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        if (e.getSource()==button) {
-            JFileChooser fileupload = new JFileChooser();
-            int response = fileupload.showOpenDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File file = new File(fileupload.getSelectedFile().getAbsolutePath());
-                FilesUtilites.UploadFile(file);
-            }
+public class UploadFrame extends JPanel{
+    //GUI Elements
+    private final JButton button = new JButton("Upload File");
+    public Scanner SupplierFile = null;
+    public Scanner ProductFile = null;
+    /*
+     * Inner class that accesses button
+     */
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(final ActionEvent e) {
+            if (e.getSource()==button) {
+                JFileChooser fileupload = new JFileChooser();
+                int response = fileupload.showOpenDialog(null);
+                if (response==JFileChooser.APPROVE_OPTION) {
+                    File file = new File(fileupload.getSelectedFile().getAbsolutePath());
+                    try {
+                        if (file.getName().equals("SupplierFile.txt")) {
+                            SupplierFile = new Scanner(file);
+                            FilesUtilites.SupplierPrint(SupplierFile);
+                        } else if (file.getName().equals("ProductFile.txt")) {
+                            ProductFile = new Scanner(file);
+                            FilesUtilites.ProductPrint(ProductFile);
+                        } else {
+                            System.out.println("Invalid file. Please select the proper files.");
+                        }   
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            } 
+            if (SupplierFile!=null && ProductFile!=null) {
+                System.out.println("it works!");
+            } 
         }
+    }
+    
+    /*
+     * Panel constructor. Lays out GUI elements then applies their listeners.
+     */
+    public UploadFrame() {
+        this.add(this.button);
+        this.button.setFocusable(false);    
+        this.button.addActionListener(new ButtonListener());
+
     }
 }
